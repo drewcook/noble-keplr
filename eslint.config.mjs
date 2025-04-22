a57bedcdba659170a123e58524b7161dafef46e9
Drew Cook <drew@dco.dev>
2025-04-21 20:48:59 -0600

    fix: lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,24 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
 	...compat.config({
-		extends: [
-			'next/core-web-vitals',
-			'next/typescript',
-			'prettier',
-			// 'plugin:@tanstack/eslint-plugin-query/recommended',
-		],
-		plugins: [/*'@tanstack/query',*/ 'simple-import-sort', 'prettier'],
+		extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+		plugins: ['simple-import-sort', 'prettier'],
 		rules: {
 			// Prettier
 			'prettier/prettier': 'warn',
 			// Import Sorts
 			'simple-import-sort/imports': 'error',
 			'simple-import-sort/exports': 'warn',
-			// React Query
-			// '@tanstack/query/exhaustive-deps': 'error',
-			// '@tanstack/query/no-deprecated-options': 'error',
-			// '@tanstack/query/prefer-query-object-syntax': 'error',
-			// '@tanstack/query/stable-query-client': 'error',
 			// TypeScript
 			'@typescript-eslint/no-unused-vars': [
 				'warn',
